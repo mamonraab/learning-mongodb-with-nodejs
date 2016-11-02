60e6c686c35a4f4003cba97bd9ec6390c7b60ceb
almaamoon rasool <magcanlight@hotmail.com>
2016-11-02 12:32:47 +0300

    desgin new

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ var app = web();
 var port = process.env.PORT || 3000;
 var todos = [];
 var DB = {};
+var ECT = require('ect');
+var ectRenderer = ECT({ watch: true, root: __dirname + '/r3', ext : '.ect' });
 var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/tododb';
-require('events').EventEmitter.prototype._maxListeners = 20;
+
 var xssFilters = require('xss-filters');
 var request = require("request");
 const crypto = require('crypto');
@@ -38,7 +40,13 @@ app.use(formidable());//working with post request
 req.fields; // contains non-file fields
  req.files; // contains files
 */
- 
+ app.use(web.static('r32'));
+app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
+
+app.get('/mamon32', function (req, res){
+    res.render('index');
+});
 app.get('/fb', function(input, output) {
 output.header("Content-Type", "application/json; charset=utf-8");
 var url = "https://graph.facebook.com/v2.8/202409816773047?fields=feed.limit(10)%7Bfull_picture%2Cmessage%2Ccreated_time%7D&access_token=EAAKjrMtqCRwBADDc6J4DI6zk8UI649p7cimeRNNEVqZBLQ3Xnsx8Vocrs9Lu3FzhyO992Kz4iRljzu7o3seti16RLS43TLdXQyUJ0FpPBZAUPUJDNKPonfdZC5GGW8CvKCAIVV5SzhoksewpbjuNmIFiCzGeS8Se49z1BxxBQZDZD"
@@ -217,6 +225,14 @@ app.get('/r32', function(req, res) {
  
 });
 
+app.get('/rma', function(req, res) {
+
+   
+   
+   
+    res.sendFile('r32/OnePage/index.html', {root: __dirname });
+ 
+});
 
 
 app.use('/rr', web.static(__dirname + '/r3'));

32bd08794b1ade62c1bf5a9f3e503425dc33ca30
almaamoon rasool <magcanlight@hotmail.com>
2016-11-02 14:17:19 +0300

    complate all

--- facebook.js
+++ facebook.js
@@ -1,4 +1,5 @@
 var web = require('express');
+var bodyParser = require('body-parser'); //geting the medileware that parse the post
 var validator = require('validator');
 const formidable = require('express-formidable');
 //using underscore labrry
@@ -8,35 +9,51 @@ var port = process.env.PORT || 3000;
 var ECT = require('ect');
 var ectRenderer = ECT({ watch: true, root: __dirname + '/r32', ext : '.ect' });
 var request = require("request");
+var path = require('path');
+app.use(web.static(path.join(__dirname, 'r32')));
+app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
+
+process.setMaxListeners(0);
 app.use(formidable());//working with post request
 /*
 req.fields; // contains non-file fields
  req.files; // contains files
 */
-app.use(web.static('r32'));
+
 app.set('view engine', 'ect');
 app.engine('ect', ectRenderer.render);
 
-app.get('/view', function (req, res){
-    res.render('index');
-});
+
 app.get('/fb', function(input, output) {
 output.header("Content-Type", "application/json; charset=utf-8");
-var url = "https://graph.facebook.com/v2.8/202409816773047?fields=feed.limit(10)%7Bfull_picture%2Cmessage%2Ccreated_time%7D&access_token=EAAKjrMtqCRwBADDc6J4DI6zk8UI649p7cimeRNNEVqZBLQ3Xnsx8Vocrs9Lu3FzhyO992Kz4iRljzu7o3seti16RLS43TLdXQyUJ0FpPBZAUPUJDNKPonfdZC5GGW8CvKCAIVV5SzhoksewpbjuNmIFiCzGeS8Se49z1BxxBQZDZD"
+var url = "https://graph.facebook.com/v2.8/202409816773047?fields=feed.limit(10)%7Bfull_picture%2Cmessage%2Ccreated_time%7D&access_token=742912495847708|R7_vOrPKm34iLPAn7-eKq5sWBNc";
 request(url, function(error, response, body) {
 var jsonObject = JSON.parse(body);
-  output.json(jsonObject.feed.data[0]);
+ output.json(jsonObject.feed);
+
 });
 });
 
+app.get('/fb2', function(input, output) {
+//output.header("Content-Type", "application/json; charset=utf-8");
 
+var url = "https://graph.facebook.com/v2.8/202409816773047?fields=feed.limit(10)%7Bfull_picture%2Cmessage%2Ccreated_time%7D&access_token=742912495847708|R7_vOrPKm34iLPAn7-eKq5sWBNc";
+request(url, function(error, response, body) {
+var jsonObject = JSON.parse(body);
+
+    output.render('index',jsonObject.feed);
+});
+
+
+});
 
 app.get('/onepage', function(req, res) {
 
    
    
    
-    res.sendFile('r32/OnePage/index.html', {root: __dirname });
+    res.sendFile('r32/OnePage2/index.html', {root: __dirname });
  
 });
 
